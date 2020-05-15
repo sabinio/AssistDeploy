5b82e6296adcded1550035d28c89ad26ce1a6b0f
Richie Lee <richie@bzzzt.io>
2020-05-15 14:10:52 +0100

    remove another \

--- createdocs.ps1
+++ createdocs.ps1
@@ -18,7 +18,7 @@ else {
 $homeMarkdown = Resolve-Path ..\AssistDeploy.wiki\Home.md
 $markDownFiles = Get-ChildItem ..\AssistDeploy.wiki -Exclude "Home.md"
     foreach ($m in $markDownFiles) {
-        $markdownLinks += "`n[$($m.BaseName)](\$($m.BaseName))"
+        $markdownLinks += "`n[$($m.BaseName)]($($m.BaseName))"
     } 
 $homeContent = "`nWelcome to the AssistDeploy wiki!`n`nhttps://sabin.io/blog/assist-deploy-is-available-on-github/`n" + $markDownLinks
 New-Item -Path $homeMarkdown -ItemType File -Value $homeContent -Force
