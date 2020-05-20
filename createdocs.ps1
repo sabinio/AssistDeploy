c31afefc79676251e093e0d6435104f13ece1bc4
Richie Lee <richie@bzzzt.io>
2020-05-20 11:05:43 +0100

    update to creating docs

--- createdocs.ps1
+++ createdocs.ps1
@@ -3,8 +3,16 @@ $reset = $true
 Install-Module platyPS -Scope CurrentUser
 Import-Module platyPS
 Set-Location $PSScriptRoot
-
 Import-Module .\AssistDeploy.psd1 -Force
+
+if ((Test-Path ..\AssistDeploy.wiki\Home.md) -eq $false){
+    Write-Host "Cloning wiki repo..."
+    Push-Location 
+    Set-Location ..\
+    git clone https://github.com/sabinio/AssistDeploy.wiki.git
+    Pop-Location
+}
+
 if ($reset) {
     $files = Get-ChildItem .\Functions -Filter *.ps1
     foreach ($f in $files) {
@@ -18,7 +26,7 @@ else {
 $homeMarkdown = Resolve-Path ..\AssistDeploy.wiki\Home.md
 $markDownFiles = Get-ChildItem ..\AssistDeploy.wiki -Exclude "Home.md"
     foreach ($m in $markDownFiles) {
-        $markdownLinks += "`n[$($m.BaseName)]($($m.BaseName))"
+        $markdownLinks += "`n[$($m.BaseName)]($($m.BaseName))`n"
     } 
-$homeContent = "`nWelcome to the AssistDeploy wiki!`n`nhttps://sabin.io/blog/assist-deploy-is-available-on-github/`n" + $markDownLinks
+$homeContent = "`nWelcome to the AssistDeploy wiki!`n`nI've written a post about [Assist Deploy](https://sabin.io/blog/assist-deploy-is-available-on-github/) being released.`n" + $markDownLinks
 New-Item -Path $homeMarkdown -ItemType File -Value $homeContent -Force
