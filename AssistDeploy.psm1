62e7954ed955a6b2452d7938c58f10b53c0f1e5e
Richie Lee <richie@bzzzt.io>
2020-06-19 15:25:24 +0100

    remove

--- AssistDeploy.psm1
+++
@@ -1,4 +0,0 @@
-foreach ($function in (Get-ChildItem "$PSScriptRoot\functions\*.ps1"))
-{
-	$ExecutionContext.InvokeCommand.InvokeScript($false, ([scriptblock]::Create([io.file]::ReadAllText($function))), $null, $null)
-}
